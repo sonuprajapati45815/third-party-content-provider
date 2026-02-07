864413a30ff21baf92c517825153a91832352330
Sonu Prajapati <sonu.prajapati+gbtg@amexgbt.com>
2026-02-07 18:35:04 +0530

    Update views.py

--- news/views.py
+++ news/views.py
@@ -9,22 +9,24 @@ news_bp = Blueprint('news', __name__, url_prefix='/third-party-service/news')
 @news_bp.route('/latest', methods=['GET'])
 def get_news_by_query_view():
     query = request.args.get('query', 'indian politics and business')
-    cached_news = redis_client.get(f'news:latest{query}')
+    redis_key = f'news:latest{query}'
+    cached_news = redis_client.get(redis_key)
     if cached_news:
         return jsonify(json.loads(cached_news)), 200
     latest_news = get_news_by_query(query)
-    redis_client.set('news:latest', json.dumps(latest_news), ex=3600)
+    redis_client.set(redis_key, json.dumps(latest_news), ex=3600)
     return latest_news, 200
 
 @news_bp.route('/top_headline', methods=['GET'])
 def top_headline():
     category = request.args.get('category', 'politics')
     country = request.args.get('country', 'india')
-    cached_news = redis_client.get('top_headline:category:{}:country:{}'.format(category, country))
+    redis_key = 'top_headline:category:{}:country:{}'.format(category, country)
+    cached_news = redis_client.get(redis_key)
     if cached_news:
         return jsonify(json.loads(cached_news)), 200
     top_headline = get_news_by_top_headline(country, category)
-    redis_client.set('news:latest', json.dumps(top_headline), ex=3600)
+    redis_client.set(redis_key, json.dumps(top_headline), ex=3600)
     return top_headline, 200
 
 
